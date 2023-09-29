09861168adc218c8ca6beca2ee83c7a05689ca5b
Rado <radoslav.minchev@gmail.com>
2023-09-30 00:51:39 +0300

    Refactor engineer.py for clarity and efficiency
    This commit refactors the engineer.py file to improve code clarity and efficiency. The StepsConfig import and the get_improve_prompt function, which were not being used, have been removed. The run_engineer and run_engineer_improve functions have been updated with detailed docstrings for better understanding of their functionality. The set_improve_filelist function call has been removed from run_engineer_improve as it was redundant. The set_improved_file_list function has been updated with a docstring for better understanding of its purpose.

--- fast-api/engineer.py
+++ fast-api/engineer.py
@@ -4,8 +4,6 @@ from initializer import initialize
 
 from gpt_engineer.steps import (
     STEPS,
-    Config as StepsConfig,
-    get_improve_prompt,
     improve_existing_code,
     set_improve_filelist,
 )
@@ -17,6 +15,15 @@ operation_status = {}
 def run_engineer(app_name, message, current_user, steps_config):
     """
     Function to run the engineer.
+    This function initializes the AI and databases, sets the input prompt, and then runs the steps defined in the steps_config.
+    The progress of the operation is updated in the global dictionary operation_progress.
+    Once all steps are completed, the status of the operation is updated in the global dictionary operation_status.
+
+    Parameters:
+    app_name (str): The name of the app.
+    message (str): The input prompt.
+    current_user (str): The current user.
+    steps_config (StepsConfig): The configuration of the steps to be run.
     """
     ai, dbs = initialize(app_name, current_user)
 
@@ -38,6 +45,15 @@ def run_engineer(app_name, message, current_user, steps_config):
 def run_engineer_improve(app_name, message, current_user, steps_config):
     """
     Function to run the engineer.
+    This function initializes the AI and databases, sets the input prompt, and then runs the steps defined in the steps_config.
+    The progress of the operation is updated in the global dictionary operation_progress.
+    Once all steps are completed, the status of the operation is updated in the global dictionary operation_status.
+
+    Parameters:
+    app_name (str): The name of the app.
+    message (str): The input prompt.
+    current_user (str): The current user.
+    steps_config (StepsConfig): The configuration of the steps to be run.
     """
 
     app_base_path = (
@@ -53,8 +69,6 @@ def run_engineer_improve(app_name, message, current_user, steps_config):
 
     set_improved_file_list(dbs)
 
-    set_improve_filelist(ai, dbs)
-
     improve_existing_code(ai, dbs)
 
     operation_status[app_name] = "Completed"
@@ -63,10 +77,13 @@ def run_engineer_improve(app_name, message, current_user, steps_config):
 def set_improved_file_list(dbs):
     """
     Function to list all files in a given directory and write them down with their absolute paths to a file called file_list.txt.
+    This function is used to keep track of all the files in the workspace.
+
+    Parameters:
+    dbs (DBs): The databases.
     """
     with open(dbs.project_metadata.path / "file_list.txt", "w") as file_obj:
         for root, dirs, files in os.walk(dbs.workspace.path):
             for file in files:
                 file_path = os.path.join(dbs.workspace.path, file)
                 file_obj.write(f"{file_path}\n")
-
