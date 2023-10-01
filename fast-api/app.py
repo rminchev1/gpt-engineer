7929009ca7efeb45c6556d709f7009a7641db67c
Rado <radoslav.minchev@gmail.com>
2023-10-01 17:14:38 +0300

    Remove Jinja2Templates and homepage route
    This commit removes the Jinja2Templates import and the associated templates variable from the fast-api/app.py file. The route serving the homepage has also been removed. This change is part of a larger effort to transition away from server-side rendering and towards a more API-focused architecture. The static files mount has also been removed as it is no longer necessary without the homepage route.

--- fast-api/app.py
+++ fast-api/app.py
@@ -14,7 +14,6 @@ from fastapi.security import OAuth2PasswordBearer
 from starlette.requests import Request
 from starlette.responses import FileResponse, JSONResponse
 from routes import router
-from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
 
@@ -32,8 +31,6 @@ operation_progress = {}
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-# Jinja2 templates
-templates = Jinja2Templates(directory="templates")
 
 # Load environment variables
 def load_env_if_needed():
@@ -70,13 +67,3 @@ async def add_process_time_header(request: Request, call_next):
     if "Authorization" in request.headers:
         response.headers["Authorization"] = request.headers["Authorization"]
     return response
-
-@app.get("/", response_class=HTMLResponse)
-async def read_item(request: Request):
-    """
-    Function to serve the home page.
-    """
-    return templates.TemplateResponse("index.html", {"request": request})
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
